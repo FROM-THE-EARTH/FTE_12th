a5d9ffea515ede8e16a23a8d25493e2ae54926e4
hiro712 <abe15263076@gmail.com>
2021-12-25 22:32:56 +0900

    Update GPS.cpp

--- IZU2022/GPS.cpp
+++ IZU2022/GPS.cpp
@@ -9,6 +9,8 @@ Serial pc(USBTX, USBRX);
 GPS gps(PA_9,PA_10); //GPSの初期化(tx,rx)mbed:D1,D0
 IM920 im920(PA_2,PA_3,PF_0,PB_3); //IM920の初期化(tx,rx,busy,reset)mbed:A7,A2,D7,D13 arduinoシールド:D9,D8,D10,- *resetは使用しなかった*
 
+char a[256] = "katokato";
+
 void imSend(char *send){//無線で送信する関数
     im920.send(send,strlen(send)+1);
     pc.printf(send);
@@ -28,7 +30,7 @@ void getGPS(){//GPSの値を取得してsendDatesに値を入れる関数
     gps.GetData();
     if(gps.readable == true){
        sendDatas(gps.latitude, gps.longtitude, gps.altitude, gps.time);
-       imSend("katokato");
+       imSend(a);
     }
 }
 
@@ -40,7 +42,8 @@ int main(){
     //pc.baud(115200); //mbedのボーレート（必要に応じて)
     gps.attach(getGPS);//割り込み処理設定(関数名)
     while(1){
-        imSend("aaaaaaaaa");
+        int b[256] = "abcde";
+        imSend(b);
         //gps.GetDefault(); //元のデータ
         //pc.printf("%c",gps.datadefault);  //書き込み
     }
