3af66c41e61ff8d31117ff1092c344f4620ea657
KATO SHUNSUKE <kato.shunsuke.p7@dc.tohoku.ac.jp>
2022-02-12 18:24:52 +0900

    Update Caliblation_sample.cpp

--- cansat/Caliblation_sample.cpp
+++ cansat/Caliblation_sample.cpp
@@ -1,23 +1,31 @@
 //これはキャリブレーションの計算式です
-/*
-    if(maxMagX < medianMagX){
-        medianMagX = maxMagX;
+/*  
+
+    while(millis() < 10*1000){//キャリブレーションが終わるまでを現段階では時間で判断している
+    getMpu();
+    millisStart();
+
+    if(maxMagX < MagX){
+        MagX = maxMagX;
     }
-    if(minMagX > medianMagX){
-        medianMagX = minMagX;
+    if(minMagX > MagX){
+        MagX = minMagX;
     }
-    if(maxMagY < medianMagY){
-        medianMagY = maxMagY;
+    if(maxMagY < MagY){
+        MagY = maxMagY;
     }
-    if(minMagY > medianMagY){
-        medianMagY = minMagY;
+    if(minMagY > MagY){ 
+        MagY = minMagY;
     }
-        
+    
+    }
         
     centerMagX = (maxMagX + minMagX)/2;
     centerMagY = (maxMagY + minMagY)/2;
    
-        
+   //この後の処理は別の関数にまとめた方がいいと思う。常に発動しておくべき関数だから
+   //4分岐にしているのは角度の表示方法を柔軟に変えられるから
+   //北からの角度を0~360で表すのか、0~180,0~-180で表すのか。ちなみにこのコードは北を0度として0~360で表してる
     switch(code){
             case 0:if(MagX-centerMagX>0 && MagY-centerMagY>=0){
                         angle_from_north = 90 - (180/pi)*atan((MagY - centerMagY)/(MagX - MagX));
@@ -37,4 +45,8 @@
                     }break;
                     
          }
+
+    //return angle_from_northとか、たぶんvoid関数にした方がいいかも
+
+    
 */
