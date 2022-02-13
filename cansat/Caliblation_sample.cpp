2b613f8612711d10dfec72731ebd699ca4089716
KATO SHUNSUKE <kato.shunsuke.p7@dc.tohoku.ac.jp>
2022-02-13 13:54:25 +0900

    Update Caliblation_sample.cpp

--- cansat/Caliblation_sample.cpp
+++ cansat/Caliblation_sample.cpp
@@ -1,52 +1,114 @@
-//これはキャリブレーションのコードです
-/*  
+#include "mbed.h"
+#include "mpu9250_i2c.h"
+#include "millis.h"
+#include "math.h"
 
-    while(millis() < 10*1000){//キャリブレーションが終わるまでを現段階では時間で判断している
-    getMpu();
-    millisStart();
+#define pi 3.141592653
 
-    if(maxMagX < MagX){
-        MagX = maxMagX;
-    }
-    if(minMagX > MagX){
-        MagX = minMagX;
-    }
-    if(maxMagY < MagY){
-        MagY = maxMagY;
-    }
-    if(minMagY > MagY){ 
-        MagY = minMagY;
-    }
+I2C i2cBus(D4,D5);
+mpu9250 mpu(i2cBus, AD0_HIGH);
+RawSerial pc(USBTX,USBRX,115200);
+
+float acc[3]={};
+float mag[3]={};
+float gyro[3]={};
+
+float magX,magY;
+float centerMagX,centerMagY;
+float maxMagX,minMagX;
+float maxMagY,minMagY;
+
+float Azimuth;//方位角
+
+void getMpu();
+void calibration();
+void culcAzimuth();
+void motorForward();
+
+int main(){
+    calibration();//一回のみ発動する関数
+    culcAzimuth();//常に発動すべき関数、これを発動すれば変数Azimuthに北からの角度が格納される
+    pc.printf("angle from north = %f\n",Azimuth);
+}
+
+void getMpu(){
+    mpu.setAccLPF(NO_USE);
+    mpu.setAcc(_16G);
+    mpu.getAcc(acc);
+    mpu.getGyro(gyro);
+    mpu.getMag(mag);
     
-    }
+    magX = mag[0];
+    magY = mag[1];
+}
+
+void calibration(){
+    
+    bool complete_calibration = false;//キャリブレーションの完了を判断する変数
+    
+    while(complete_calibration != true){
+        while(millis()<15*1000){
+            getMpu();
+            millisStart();
         
+            if(maxMagX < magX){
+                maxMagX = magX;
+            }
+            if(minMagX > magX){
+                minMagX = magX;
+            }
+            if(maxMagY < magY){
+                maxMagY = magY;
+            }
+            if(minMagY > magY){
+                minMagY = magY;
+            }
+        }
+        
+        if(maxMagX - minMagX > 50 && maxMagY - minMagY >50){
+            complete_calibration = true;//キャリブレーション完了
+        }else{
+            motorForward();//少し移動してからまたキャリブレーション
+            wait(10);
+            complete_calibration = false;
+        }
+    }
+            
     centerMagX = (maxMagX + minMagX)/2;
     centerMagY = (maxMagY + minMagY)/2;
-   
-   //この後の処理は別の関数にまとめた方がいいと思う。常に発動しておくべき関数だから
-   //4分岐にしているのは角度の表示方法を柔軟に変えられるから
-   //北からの角度を0~360で表すのか、0~180,0~-180で表すのか。ちなみにこのコードは北を0度として0~360で表してる
+    
+}
+
+void culcAzimuth(){
+    getMpu();//getMpu()の複数発動に注意
+    int code = 0;
     switch(code){
-            case 0:if(MagX-centerMagX>0 && MagY-centerMagY>=0){
-                        angle_from_north = 90 - (180/pi)*atan((MagY - centerMagY)/(MagX - MagX));
+            case 0:if(magX-centerMagX>0 && magY-centerMagY>=0){
+                        Azimuth = 90 - (180/pi)*atan((magY - centerMagY)/(magX - centerMagX));
                         code  = 0;
                    }break;
-            case 1:if(MagX-centerMagX<0 && MagY-centerMagY>=0){
-                        angle_from_north = 270 - (180/pi)*atan((MagY - centerMagY)/(MagX - centerMagX));
+            case 1:if(magX-centerMagX<0 && magY-centerMagY>=0){
+                        Azimuth = 270 - (180/pi)*atan((magY - centerMagY)/(magX - centerMagX));
                         code = 1;
                     }break;
-            case 2:if(MagX-centerMagX<0 && MagY-centerMagY<=0){
-                        angle_from_north = 270 -  (180/pi)*atan((MagY - centerMagY)/(MagX - centerMagX));
+            case 2:if(magX-centerMagX<0 && magY-centerMagY<=0){
+                        Azimuth = 270 -  (180/pi)*atan((magY - centerMagY)/(magX - centerMagX));
                         code = 2;
                     }break;
-            case 3:if(MagX-centerMagX>0 && MagY-centerMagY<=0){
-                        angle_from_north = 90 - (180/pi)*atan((MagY - centerMagY)/(MagX - centerMagX));
+            case 3:if(magX-centerMagX>0 && magY-centerMagY<=0){
+                        Azimuth = 90 - (180/pi)*atan((magY - centerMagY)/(magX - centerMagX));
                         code = 3;
                     }break;
                     
          }
+}
 
-    //return angle_from_northとか、たぶんvoid関数にした方がいいかも
-
+void motorForward(){
+    ;//前進関数
+}
     
-*/
+    
+        
+        
+        
+        
