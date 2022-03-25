fd033d89c98a70c05e3cae29627e0920cec64cc7
KATO SHUNSUKE <kato.shunsuke.p7@dc.tohoku.ac.jp>
2022-03-25 23:48:38 +0900

    Create IM920_conf.h

---
+++ IZU2022/modules/IM920/IM920_conf.h
@@ -0,0 +1,16 @@
+#ifndef _IM920_conf_h_
+#define _IM920_conf_h_
+
+//#define DEBUG
+//#define DEBUG_DUMP
+
+#define IM920_BAUD 19200
+
+#define CFG_BUF_SIZE 150
+#define CFG_DATA_SIZE 64
+#define CFG_CMD_SIZE 16
+
+#define DEFAULT_WAIT_RESP_TIMEOUT 100
+#define CFG_TIMEOUT 500
+
+#endif
