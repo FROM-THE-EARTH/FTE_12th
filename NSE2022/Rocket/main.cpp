58df47e0bccf46a2e7fca00e6ae3c4cb3977bbc2
KATO SHUNSUKE <kato.shunsuke.p7@dc.tohoku.ac.jp>
2022-05-05 19:19:29 +0900

    Update main.cpp

---
+++ NSE2022/Rocket/main.cpp
@@ -0,0 +1,315 @@
+#include<Wire.h>
+#include <Adafruit_BMP085.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+
+Adafruit_BMP085 bmp180;
+
+#define Addr_Accl 0x19 
+#define Addr_Gyro 0x69  
+#define Addr_Mag 0x13
+
+#define FLIGHT_PIN 25
+
+float xAccl = 0.00;
+float yAccl = 0.00;
+float zAccl = 0.00;
+float xGyro = 0.00;
+float yGyro = 0.00;
+float zGyro = 0.00;
+int   xMag  = 0;
+int   yMag  = 0;
+int   zMag  = 0;
+
+float pressure;
+float temperature;
+float Altitude;
+
+int val = 0;
+float PWM;
+
+char buf1[50];
+char DATA[50];
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+  InitBMX055();
+  InitBMP180();
+  pinMode(FLIGHT_PIN,INPUT);
+  pinMode(26,OUTPUT);
+  
+  ledcSetup(0,50,10);
+  ledcAttachPin(26,0);
+
+  if(!SD.begin()){
+   Serial.println("Card Mount Failed");
+  }
+  
+  writeFile(SD, "/LOG.csv", "hello ");
+
+  delay(300);
+}
+
+void loop()
+{
+  val = digitalRead(FLIGHT_PIN);
+  /*
+  Serial.println(val);
+  servoWrite(0);
+  delay(1000);
+  servoWrite(180);
+  delay(1000);
+  */
+
+  
+  getBMX055();
+  getBMP180();
+
+/*
+  sprintf(DATA,"%f,%f,%f\n",xAccl,yAccl,zAccl);
+  appendFile(SD,"/ACCLOG.csv",DATA);
+*/
+ 
+  dtostrf(xAccl,5,2,buf1);
+  sprintf(DATA,"%s\n",buf1);
+  appendFile(SD,"/LOG.csv",DATA);
+ 
+ 
+  /*
+  Serial.print(xAccl);
+  Serial.print(yAccl);
+  Serial.print(zAccl);   
+  
+  Serial.print(xGyro);
+  Serial.print(yGyro);
+  Serial.print(zGyro);
+  
+  Serial.print(xMag);
+  Serial.print(yMag); 
+  Serial.print(zMag);
+ 
+  Serial.println(pressure);
+  Serial.println(temperature);
+  */
+
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){//to write first information to the new file
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){//to add information to the previous file
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void servoWrite(float angle){
+  
+  float k = angle*0.539;
+  PWM = k + 26;
+  
+  if(PWM > 123){
+    PWM = 123;
+  }
+
+  ledcWrite(0,PWM);
+  
+}
+
+void InitBMX055()
+{
+  
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x0F); // Select PMU_Range register
+  Wire.write(0x03);   // Range = +/- 2g
+  Wire.endTransmission();
+  delay(100);
+ 
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x10);  // Select PMU_BW register
+  Wire.write(0x08);  // Bandwidth = 7.81 Hz
+  Wire.endTransmission();
+  delay(100);
+  
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x11);  // Select PMU_LPW register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 0.5ms
+  Wire.endTransmission();
+  delay(100);
+ 
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x0F);  // Select Range register
+  Wire.write(0x04);  // Full scale = +/- 125 degree/s
+  Wire.endTransmission();
+  delay(100);
+
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x10);  // Select Bandwidth register
+  Wire.write(0x07);  // ODR = 100 Hz
+  Wire.endTransmission();
+  delay(100);
+ 
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x11);  // Select LPM1 register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 2ms
+  Wire.endTransmission();
+  delay(100);
+ 
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x83);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+  
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x01);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+ 
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4C);  // Select Mag register
+  Wire.write(0x00);  // Normal Mode, ODR = 10 Hz
+  Wire.endTransmission();
+ 
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4E);  // Select Mag register
+  Wire.write(0x84);  // X, Y, Z-Axis enabled
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x51);  // Select Mag register
+  Wire.write(0x04);  // No. of Repetitions for X-Y Axis = 9
+  Wire.endTransmission();
+
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x52);  // Select Mag register
+  Wire.write(0x16);  // No. of Repetitions for Z-Axis = 15
+  Wire.endTransmission();
+}
+
+void BMX055_Accl()
+{
+  unsigned int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Accl);
+    Wire.write((2 + i));// Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Accl, 1);// Request 1 byte of data
+    // Read 6 bytes of data
+    // xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data to 12-bits
+  xAccl = ((data[1] * 256) + (data[0] & 0xF0)) / 16;
+  if (xAccl > 2047)  xAccl -= 4096;
+  yAccl = ((data[3] * 256) + (data[2] & 0xF0)) / 16;
+  if (yAccl > 2047)  yAccl -= 4096;
+  zAccl = ((data[5] * 256) + (data[4] & 0xF0)) / 16;
+  if (zAccl > 2047)  zAccl -= 4096;
+  xAccl = xAccl * 0.0098; // range = +/-2g
+  yAccl = yAccl * 0.0098; // range = +/-2g
+  zAccl = zAccl * 0.0098; // range = +/-2g
+}
+//=====================================================================================//
+void BMX055_Gyro()
+{
+  unsigned int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Gyro);
+    Wire.write((2 + i));    // Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Gyro, 1);    // Request 1 byte of data
+    // Read 6 bytes of data
+    // xGyro lsb, xGyro msb, yGyro lsb, yGyro msb, zGyro lsb, zGyro msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data
+  xGyro = (data[1] * 256) + data[0];
+  if (xGyro > 32767)  xGyro -= 65536;
+  yGyro = (data[3] * 256) + data[2];
+  if (yGyro > 32767)  yGyro -= 65536;
+  zGyro = (data[5] * 256) + data[4];
+  if (zGyro > 32767)  zGyro -= 65536;
+
+  xGyro = xGyro * 0.0038; //  Full scale = +/- 125 degree/s
+  yGyro = yGyro * 0.0038; //  Full scale = +/- 125 degree/s
+  zGyro = zGyro * 0.0038; //  Full scale = +/- 125 degree/s
+}
+
+void BMX055_Mag()
+{
+  unsigned int data[8];
+  for (int i = 0; i < 8; i++)
+  {
+    Wire.beginTransmission(Addr_Mag);
+    Wire.write((0x42 + i));    // Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Mag, 1);    // Request 1 byte of data
+    // Read 6 bytes of data
+    // xMag lsb, xMag msb, yMag lsb, yMag msb, zMag lsb, zMag msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+// Convert the data
+  xMag = ((data[1] <<5) | (data[0]>>3));
+  if (xMag > 4095)  xMag -= 8192;
+  yMag = ((data[3] <<5) | (data[2]>>3));
+  if (yMag > 4095)  yMag -= 8192;
+  zMag = ((data[5] <<7) | (data[4]>>1));
+  if (zMag > 16383)  zMag -= 32768;
+}
+
+void getBMX055(){
+  BMX055_Accl(); 
+  BMX055_Gyro();
+  BMX055_Mag();
+}
+
+void InitBMP180(){
+  if(!bmp180.begin()){
+    Serial.print("BMP180 Error");
+  }else{
+    bool startBmp = true;
+  }
+}
+
+void getBMP180(){
+  pressure = bmp180.readPressure();
+  temperature = bmp180.readTemperature();
+  float changePressure = pressure/100;
+  float ratio = (1012.25/changePressure);
+  float absoluteTemperature = temperature + 273.15;
+  Altitude = (pow(ratio,1/5.257)-1)/0.0065;
+}
