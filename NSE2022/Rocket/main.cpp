af448e3bd59db3e698da25f39f8ca5d82dfb6bdc
KATO SHUNSUKE <kato.shunsuke.p7@dc.tohoku.ac.jp>
2022-05-24 00:18:54 +0900

    Update main.cpp

--- NSE2022/Rocket/main.cpp
+++ NSE2022/Rocket/main.cpp
@@ -3,16 +3,17 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
-
+#include "CAN.h"
 
 Adafruit_BMP085 bmp180;
 
 #define Addr_Accl 0x19 
 #define Addr_Gyro 0x69  
 #define Addr_Mag 0x13
-
 #define FLIGHT_PIN 25
 
+//BMX055
+float tmp;
 float xAccl = 0.00;
 float yAccl = 0.00;
 float zAccl = 0.00;
@@ -23,63 +24,155 @@ int   xMag  = 0;
 int   yMag  = 0;
 int   zMag  = 0;
 
+//BMP180
 float pressure;
 float temperature;
 float Altitude;
+float maxAltitude = 0;
 
+//flightpin
 int val = 0;
-float PWM;
+//servo
+float PWM1;
+float PWM2;
 
-char buf1[50];
-char DATA[50];
+//data for converting
+char buf1[20];
+char buf2[20];
+char buf3[20];
+char buf4[20];
+char buf5[20];
+char buf6[20];
+char buf7[20];
+char buf8[20];
+char buf9[20];
+char buf10[20];
+char buf11[20];
+char buf12[20];
+
+//data for SDcard
+char LOGDATA[180];
+
+//time
+int launchedTime = 0;
+
+//sequence
+int sequence = 0;
 
 void setup()
 {
   Wire.begin();
+  
   Serial.begin(9600);
+  
   InitBMX055();
   InitBMP180();
+  
   pinMode(FLIGHT_PIN,INPUT);
-  pinMode(26,OUTPUT);
+  pinMode(17,OUTPUT);
   
   ledcSetup(0,50,10);
-  ledcAttachPin(26,0);
+  ledcAttachPin(17,0);
+
+  ledcSetup(1,50,10);
+  ledcAttachPin(5,1);
 
   if(!SD.begin()){
    Serial.println("Card Mount Failed");
   }
-  
+
   writeFile(SD, "/LOG.csv", "hello ");
 
-  delay(300);
+  CAN.setPins(26,27);
+
+  if(!CAN.begin(500E3)){
+    Serial.println("CAN connection failed");
+  }
+
 }
 
 void loop()
 {
-  val = digitalRead(FLIGHT_PIN);
-  /*
-  Serial.println(val);
-  servoWrite(0);
-  delay(1000);
-  servoWrite(180);
-  delay(1000);
-  */
-
+  servo1Write(0);
+  servo2Write(1);
   
-  getBMX055();
-  getBMP180();
+  while(1){
+    
+    CAN.beginPacket(0x12);
+    CAN.write(byte(val));
+    CAN.endPacket();
 
-/*
-  sprintf(DATA,"%f,%f,%f\n",xAccl,yAccl,zAccl);
-  appendFile(SD,"/ACCLOG.csv",DATA);
-*/
+    val = digitalRead(FLIGHT_PIN);
+
+    if(val == 0){//detect launch
+      sequence = 1;
+      break;
+    }
+  }
+
+
+  launchedTime = millis();
+
+  while(sequence = 1){
+
+   //get I2C
+    getBMX055();
+    getBMP180();
+
+    //check flightpin
+    val = digitalRead(FLIGHT_PIN);
+
+    //****send by IM920****//
+    CAN.beginPacket(0x12);
+    CAN.write(byte(val));
+    CAN.write(byte(Altitude));
+    CAN.endPacket();
+
+    //****write to SD****//
+    CAN.beginPacket(0X19);
+    
+    //time information
+    CAN.write(byte(millis()));
+    
+    //data from BMX055
+    CAN.write(byte(xAccl));
+    CAN.write(byte(yAccl));
+    CAN.write(byte(zAccl));
+    CAN.write(byte(xGyro));
+    CAN.write(byte(yGyro));
+    CAN.write(byte(zGyro));
+    CAN.write(byte(xMag));
+    CAN.write(byte(yMag));
+    CAN.write(byte(zMag));
+    
+    //data from BMP180
+    CAN.write(byte(pressure));
+    CAN.write(byte(temperature));
+    CAN.write(byte(Altitude));
+    
+    //sequence
+    CAN.write(byte(val));
+    CAN.write(byte(sequence));
+
+    //SD
+    convertData();
+    appendFile(SD,"/LOG.csv",LOGDATA);
+
+    //parachute open algorithm
+    if(sequence == 1 && (millis() - launchedTime > 15*1000 || maxAltitude - Altitude > 10)){
+      servo1Write(90);
+      servo2Write(90);
+      sequence = 2;
+    }else if(sequence == 2 && millis() - launchedTime > 180*1000){
+      sequence = 3;
+      break;
+    }
+    
+  }
+
+
  
-  dtostrf(xAccl,5,2,buf1);
-  sprintf(DATA,"%s\n",buf1);
-  appendFile(SD,"/LOG.csv",DATA);
- 
- 
-  /*
+  /*   code to check BMX055 and BMP180 
   Serial.print(xAccl);
   Serial.print(yAccl);
   Serial.print(zAccl);   
@@ -98,6 +191,25 @@ void loop()
 
 }
 
+void convertData(){
+  
+  dtostrf(xAccl,5,2,buf1);
+  dtostrf(yAccl,5,2,buf2);
+  dtostrf(zAccl,5,2,buf3);
+  dtostrf(xGyro,5,2,buf4);
+  dtostrf(yGyro,5,2,buf5);
+  dtostrf(zGyro,5,2,buf6);
+  dtostrf(xMag,5,2,buf7);
+  dtostrf(yMag,5,2,buf8);
+  dtostrf(zMag,5,2,buf9);
+
+  dtostrf(pressure,5,2,buf10);
+  dtostrf(temperature,5,2,buf11);
+  dtostrf(Altitude,5,2,buf12);
+
+  sprintf(LOGDATA,"%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",xAccl,yAccl,zAccl,xGyro,yGyro,zGyro,xMag,yMag,zMag,pressure,temperature,Altitude);
+
+}
 void writeFile(fs::FS &fs, const char * path, const char * message){//to write first information to the new file
     Serial.printf("Writing file: %s\n", path);
 
@@ -118,6 +230,7 @@ void appendFile(fs::FS &fs, const char * path, const char * message){//to add in
     Serial.printf("Appending to file: %s\n", path);
 
     File file = fs.open(path, FILE_APPEND);
+    
     if(!file){
         Serial.println("Failed to open file for appending");
         return;
@@ -128,20 +241,35 @@ void appendFile(fs::FS &fs, const char * path, const char * message){//to add in
         Serial.println("Append failed");
     }
     file.close();
+    
 }
 
-void servoWrite(float angle){
+void servo1Write(float angle1){
   
-  float k = angle*0.539;
-  PWM = k + 26;
+  float k = angle1*0.539;
+  PWM1 = k + 26;
   
-  if(PWM > 123){
-    PWM = 123;
+  if(PWM1 > 123){
+    PWM1 = 123;
   }
 
-  ledcWrite(0,PWM);
+  ledcWrite(0,PWM1);
   
 }
+
+void servo2Write(float angle2){
+  
+  float k = angle2*0.539;
+  PWM2 = k + 26;
+  
+  if(PWM2 > 123){
+    PWM2 = 123;
+  }
+
+  ledcWrite(1,PWM2);
+  
+}
+
 
 void InitBMX055()
 {
@@ -312,4 +440,7 @@ void getBMP180(){
   float ratio = (1012.25/changePressure);
   float absoluteTemperature = temperature + 273.15;
   Altitude = (pow(ratio,1/5.257)-1)/0.0065;
+  if(maxAltitude < Altitude){
+    maxAltitude = Altitude;
+  }
 }
