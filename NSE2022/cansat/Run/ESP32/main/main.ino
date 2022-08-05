f5ad2d6f89beebb152a4ae8ab1c09c65c9af7935
KATO SHUNSUKE <kato.shunsuke.p7@dc.tohoku.ac.jp>
2022-08-06 02:19:19 +0900

    Update main.ino

--- NSE2022/cansat/Run/ESP32/main/main.ino
+++ NSE2022/cansat/Run/ESP32/main/main.ino
@@ -1,28 +1,26 @@
-HardwareSerial im920(1);//2,15(TX,RX)
 HardwareSerial rasp(2);//17,16
 
 const int INDEX_OF_STREAMS = 4;
 
-const int RF = 33;
-const int RB = 4; //GPIO0は常に0.24Vくらい出してる(電源系と繋がっているため)ピンの選定ミスですごめんなさい
-const int LF = 26;
-const int LB = 25;
-const int SERVO = 32;
-const int IM_BUSY = 14;
+const int RF = 4;
+const int RB = 32;
+const int LF = 25;
+const int LB = 26;
+const int SERVO = 33;
 const int Kp = 256/180;
 
 int splitData(String dataString, String* dst); //文字列を分割する関数
 void setMove(float dir); //モーター制御関数
+void servoWrite(float angle);
 
 String incomingStream; //ラズパイからUARTで送られてくる生文字列
 String streams[INDEX_OF_STREAMS]; //incomingStreamを分割した文字列
 float separatedData[INDEX_OF_STREAMS]; //streams[]をdouble型にした配列
 
 
-
-
 void setup(){
   //UARTの設定
+<<<<<<< HEAD
   Serial.begin(19200);
   im920.begin(19200,SERIAL_8N1,15,2); //UART1はこのように書かなければ使えない
   /*
@@ -35,6 +33,9 @@ void setup(){
     if(cmd == "OK\r") break;
   }
   */
+=======
+  Serial.begin(115200);
+>>>>>>> cb92e3c0f2c0874a498f0ef68e1a6090c0a26cb3
   rasp.begin(9600);
 
   //ピンの設定
@@ -43,16 +44,13 @@ void setup(){
   pinMode(LF, OUTPUT);
   pinMode(LB, OUTPUT);
   pinMode(SERVO, OUTPUT);
-  pinMode(IM_BUSY, OUTPUT);
-
-  digitalWrite(IM_BUSY, HIGH);
 
   //PWMの設定
   ledcSetup(0, 12800, 10); //使用するタイマーのチャネルと周波数の設定
   ledcSetup(2, 12800, 10); //10bit分解能 max1024
   ledcSetup(4, 12800, 10);
   ledcSetup(6, 12800, 10);
-  ledcSetup(8, 12800, 10);
+  ledcSetup(8, 50, 10);
   ledcAttachPin(RF, 0); //チャネル0へ接続
   ledcAttachPin(RB, 2);
   ledcAttachPin(LF, 4);
@@ -62,7 +60,7 @@ void setup(){
   ledcWrite(2,0);
   ledcWrite(4,0);
   ledcWrite(6,0);
-  ledcWrite(8,0);
+  servoWrite(0);
   
   separatedData[1] = 0.0;
 }
@@ -81,24 +79,9 @@ void loop(){
       Serial.println("ESP32: error: splitData()");
     }  
   }
-  setMove(separatedData[1]);
-  char imSendData[64]; //IM920に送る文字列
-  char buf[64];
-  incomingStream.toCharArray(buf, 64); //Stringをcharに変更
-  sprintf(imSendData, "ECIO\r\nTXDA %s\r\n", buf); //送信コマンドを結合
-  Serial.println("IM920: send ECIO");
-  Serial.print("IM920: send TXDA ");
   Serial.println(incomingStream);
-  im920.print(imSendData);
-  delay(500);
-  while(im920.available()){
-    Serial.println("IM920: port is available");
-    String message = im920.readStringUntil('\n'); //改行コードが来るまで一気に読み込む
-    Serial.print("IM920: the response is ");
-    Serial.println(message);
-  }
+  setMove(separatedData[1]);
 }
-
 
 
 int splitData(String dataString, String* dst){ //文字列を分割する関数
@@ -120,29 +103,43 @@ int splitData(String dataString, String* dst){ //文字列を分割する関数
 
 void setMove(float dir){//モーター制御関数
   if(dir == 0.0){ //phase0
-    Serial.println("ESP32: FLYING NOW");
+    Serial.println("ESP32: FLYING NOW OR UART IS NOT READABLE");
     ledcWrite(0,0); //RF
     ledcWrite(2,0); //RB
     ledcWrite(4,0); //LF
     ledcWrite(6,0); //LB
+    servoWrite(0); //SERVO
   }else if(dir == 360){ //phase1
     Serial.println("ESP32: PARA OPENING");
-    ledcWrite(8, 1024);
+    ledcWrite(0,0); //RF
+    ledcWrite(2,0); //RB
+    ledcWrite(4,0); //LF
+    ledcWrite(6,0); //LB
+    servoWrite(180); //SERVO
   }else if(dir == -360.0){ //停止コマンドならば、
     Serial.println("ESP32: STOP");
     ledcWrite(0,0); //RF
     ledcWrite(2,0); //RB
     ledcWrite(4,0); //LF
     ledcWrite(6,0); //LB
+    servoWrite(0); //SERVO
   }else{
     if(dir>0){
       Serial.println("ESP32: LEFT");
     }else if(dir<0){
       Serial.println("ESP32: RIGHT");
     }
-    ledcWrite(0,512-dir*Kp);
+    ledcWrite(0,767-dir*Kp); //direction=-180で最大1023
     ledcWrite(2,0);
-    ledcWrite(4,512+dir*Kp);
+    ledcWrite(4,767+dir*Kp);
     ledcWrite(6,0);
+    servoWrite(0);
   }
+}
+
+void servoWrite(float angle){
+  float k = angle*0.539;
+  float pwm =  k + 26;
+  if(pwm > 123) pwm = 123;
+  ledcWrite(8,pwm);
 }
